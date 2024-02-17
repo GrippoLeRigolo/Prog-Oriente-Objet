e3956513a4a1eafab87f8f1826b043dbf279bfbc
Hippolyte <maindron.hippolyte@gmail.com>
2024-02-17 23:08:02 +0100

    modif sur main du TP2

--- TD2/src/TD2/application/main.java
+++ TD2/src/TD2/application/main.java
@@ -1,4 +1,5 @@
 package TD2.application;
+
 import TD2.metier.*;
 
 public class main {
@@ -6,13 +7,13 @@ public class main {
 	public static void main(String[] args) {
 		Personne p1 = new Personne("Raphael", "Crapet", Genre.HOMME, 19);
 		Personne p2 = new Personne("Tom", "Charpentier", Genre.HOMME, 19);
-		
-		Personne mere_Tom = new Personne("Pablette","Cadet", Genre.FEMME, 39);
-		Personne pere_Tom = new Personne("Rheda","Pelardy", Genre.HOMME, 29);
+
+		Personne mere_Tom = new Personne("Pablette", "Cadet", Genre.FEMME, 39);
+		Personne pere_Tom = new Personne("Rheda", "Pelardy", Genre.HOMME, 29);
 
 		p2.setPere(pere_Tom);
 		p2.setMere(mere_Tom);
-		
+
 		p1.afficher();
 		p2.afficherParents();
 	}
